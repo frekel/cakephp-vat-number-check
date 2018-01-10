ffb51f0fb186f9e1701852e9ce6f201c78678b06
Frank van der Stad <fvdstad@oefenweb.nl>
2018-01-10 16:50:01 +0100

    Change routes

--- config/routes.php
+++ config/routes.php
@@ -1,2 +1,11 @@
 <?php
-Router::parseExtensions('json');
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+use Cake\Routing\Route\InflectedRoute;
+
+Router::plugin('VatNumberCheck', ['path' => '/vat_number_check'], function (RouteBuilder $routes) {
+    $routes->fallbacks(InflectedRoute::class);
+});
+
+Cake\Routing\Router::extensions('json');
+
